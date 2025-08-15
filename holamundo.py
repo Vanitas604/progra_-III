9546791298f5f34c49f47cb84b90eadcfee319ae
Jennifer_baires <jennferbaires@gmail.com>
2025-08-15 09:48:33 -0600

    Práctica, hola mundo y mayor 15-08

--- holamundo.py
+++ holamundo.py
@@ -1,3 +1,4 @@
 #entrada
+
 nombre = input("Hola como te llamas: ")
 print("Bienvenido a la clase de Programación III "+nombre)
--- mayor.py
+++ mayor.py
@@ -2,6 +2,7 @@
 num1 = int(input("Ingresa el primer número: "))
 num2 = int(input("Ingresa el segundo número: "))
 
+
 # Comparar y mostrar el mayor
 if num1 > num2:
     print(f"El número mayor es : ",num1)
